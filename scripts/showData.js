5a3161232cd883f2eee65cd0c50f1dea434dad57
Anmolroan <90396759+Anmolroan@users.noreply.github.com>
2021-10-20 17:32:44 +0530

    latest page added

--- scripts/showData.js
+++ scripts/showData.js
@@ -17,17 +17,31 @@ data.forEach(({strMealThumb,strMeal,idMeal}) => {
 });
 }
 function random(container,data){
-    data.forEach(({strMealThumb,strMeal,idMeal}) => {
+    data.forEach((el) => {
         let div=document.createElement("div")
         let img=document.createElement("img")
         let title=document.createElement("p")
-        let id=document.createElement("p")
-        img.src=strMealThumb;
-        title.innerText=strMeal;
-        id.innerText= "Meal Id : "+idMeal;
-        div.append(img,title,id)
+        // let id=document.createElement("p")
+        img.src=el.strMealThumb;
+        title.innerText=el.strMeal;
+        // information
+       
+        // console.log(el.idMeal)
+        // id.innerText= "Meal Id : "+el.idMeal;
+        div.append(img,title)
+        for(let key in el){
+            console.log(key)
+            if(key!="strMealThumb"||key!="strMeal"||el[key]!=""){
+                let id=document.createElement("p")
+                id.innerText=el[key]
+                div.append(id)
+            }
+        }
+       
         container.append(div)
     });
+     
 }
 
 export {getdata,append,random}
+// {strMealThumb,strMeal,idMeal}
